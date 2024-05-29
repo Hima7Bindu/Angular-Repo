5be324030b6a0f9f99f6992224a65d03198441b7
Hima7Bindu <himabindu.d@sakshath-technologies.com>
2024-05-29 13:17:27 +0530

    Loan-App V1

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { AppLayoutComponent } from "./layout/app.layout.component";
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot([
+            {
+                path: '', component: AppLayoutComponent,
+                children: [
+                    { path: '', loadChildren: () => import('./demo/components/dashboard/dashboard.module').then(m => m.DashboardModule) },
+                   
+                    { path: 'utilities', loadChildren: () => import('./demo/components/utilities/utilities.module').then(m => m.UtilitiesModule) },
+                    
+                    
+                   
+                ]
+            },
+            { path: 'auth', loadChildren: () => import('./demo/components/auth/auth.module').then(m => m.AuthModule) },
+            { path: 'landing', loadChildren: () => import('./demo/components/landing/landing.module').then(m => m.LandingModule) },
+           
+            { path: '**', redirectTo: '/notfound' },
+        ], { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' })
+    ],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
